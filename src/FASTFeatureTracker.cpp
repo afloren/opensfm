e648ab462bbac81bcac1b163ebb43528ce723830
Andrew Floren <andrewfloren@gmail.com>
2011-10-21 00:33:33 -0500

    Some code cleaning.

--- src/FASTFeatureTracker.cpp
+++ src/FASTFeatureTracker.cpp
@@ -56,22 +56,9 @@ FASTFeatureTracker::~FASTFeatureTracker()
 
 void FASTFeatureTracker::initialize(cv::Mat image, Map *map, double time)
 {
-	//cv::Mat_<unsigned char> bw = image;
-
 	std::vector<arma::vec2> locations;
 	featureFinder->find(image,locations);
 
-	//findFeatures(bw,keypoints);
-	/*
-	std::vector<ImagePatch*> patches;
-	for(int i=0;i<keypoints.size();i++)
-	{
-		//extract 11 by 11 patch around keypoint
-		std::vector<unsigned char> patch;
-		extract_patch(bw,keypoints[i],patch,11,0);
-		patches.push_back(new ImagePatch(patch));
-	}
-	*/
 	std::vector<Feature*> features;
 	featureExtractor->extract(image,locations,features);
 
@@ -85,88 +72,12 @@ void FASTFeatureTracker::initialize(cv::Mat image, Map *map, double time)
 
 void FASTFeatureTracker::update(cv::Mat image, Map *map, double time)
 {
-	//cv::Mat_<unsigned char> bw = image;
-
-	//std::vector<arma::vec2> keypoints;
-	//findFeatures(bw,keypoints);
-
 	std::vector<arma::vec2> locations;
 	featureFinder->find(image,locations);
-
-	/*
-	std::vector<ImagePatch*> patches;
-	for(int i=0;i<keypoints.size();i++)
-	{
-		//extract 11 by 11 patch around keypoint
-		std::vector<unsigned char> patch;
-		extract_patch(bw,keypoints[i],patch,11,0);
-		patches.push_back(new ImagePatch(patch));
-	}
-	*/
-
+	
 	std::vector<Feature*> features;
 	featureExtractor->extract(image,locations,features);
 
-	/*
-	Coordinate2DHashTable ht(image.rows,image.cols);
-	std::vector<ImagePatch*> lastPatches;
-	for(int i=0;i<map->frames.back()->projections.size();i++)
-	{
-		ht.add(i,map->frames.back()->projections[i]->position);
-		lastPatches.push_back((ImagePatch*)map->frames.back()->projections[i]->point->feature);
-	}
-
-	std::vector<int> matches;
-	for(unsigned int i=0;i<keypoints.size();i++)
-	{		
-		std::vector<unsigned int> indices;
-		ht.getInRadius(keypoints[i],30,indices);
-
-		double bestScore = 5000;
-		int bestIndex = -1;
-		for(unsigned int j=0;j<indices.size();j++)
-		{
-			double score = ZMSSD(patches[i]->patch,lastPatches[indices[j]]->patch);
-			if(score < bestScore)
-			{
-				bestScore = score;
-				bestIndex = indices[j];
-			}
-		}
-		matches.push_back(bestIndex);
-	}
-
-	std::vector<std::vector<int>> asdf(map->frames.back()->projections.size());
-	for(int i=0;i<matches.size();i++)
-	{			
-		if(matches[i] >= 0)
-		{
-			asdf[matches[i]].push_back(i);
-		}
-	}
-
-	for(int i=0;i<asdf.size();i++)
-	{
-		double bestScore = 5000;
-		int bestIndex = -1;
-		for(int j=0;j<asdf[i].size();j++)
-		{
-			double score = ZMSSD(patches[asdf[i][j]]->patch,lastPatches[i]->patch);
-			if(score < bestScore)
-			{
-				bestScore = score;
-				bestIndex = asdf[i][j];
-			}
-		}
-		for(int j=0;j<asdf[i].size();j++)
-		{
-			if(asdf[i][j] != bestIndex)
-			{
-				matches[asdf[i][j]] = -1;
-			}
-		}
-	}
-	*/
 	std::vector<Feature*> lastFeatures;
 	for(int i=0;i<map->frames.back()->points.size();i++)
 	{
--- src/main.cpp
+++ src/main.cpp
@@ -53,23 +53,6 @@ int main(int argc, char *argv[])
 
 	ft.initialize(bw1,&map,timer.getTotal());
 
-	//std::vector<cv::KeyPoint> keypoints, lastKeypoints;
-	//std::vector<std::vector<unsigned char>> patches, lastPatches;
-	//std::vector<std::vector<double>> distances;
-	//std::vector<cv::KeyPoint> keypoints0, keypoints1, keypoints2, keypoints3;
-	/*
-	cv::Mat descriptors, lastDescriptors;
-	std::vector<cv::DMatch> matches;
-	std::vector<cv::DMatch> matches2;
-	*/
-	//cv::FeatureDetector *fd = new cv::FastFeatureDetector(10,true);
-	//cv::DescriptorExtractor *de = new cv::SiftDescriptorExtractor();
-	//cv::DescriptorMatcher *dm = new cv::BruteForceMatcher<cv::L2<float>>();
-
-	//fd->detect(bw,lastKeypoints);	
-	//keypoints.resize(MIN(lastKeypoints.size(),200));
-	//de->compute(bw,keypoints,lastDescriptors);			
-
 	for(;;)
 	{		
 		timer.getElapsed();
@@ -79,8 +62,6 @@ int main(int argc, char *argv[])
 		cv::pyrDown(image0,image1);
 		cv::pyrDown(image1,image2);
 		cv::pyrDown(image2,image3);
-
-		//Frame *currentFrame = map.createFrame(image,timer.getTotal());
 		
 		cv::cvtColor(image,bw,CV_RGB2GRAY);
 		cv::cvtColor(image0,bw0,CV_RGB2GRAY);
@@ -109,160 +90,11 @@ int main(int argc, char *argv[])
 
 		cv::imshow("Image",image1);
 
-		/*
-		std::cout << "image load time: " << timer.getElapsed() << "\n";
-
-		try
-		{			
-			cv::FAST(bw2,keypoints,20,true);
-			//cv::FAST(bw0,keypoints0,20,false);
-			//cv::FAST(bw1,keypoints1,20,false);
-			//cv::FAST(bw2,keypoints2,20,false);
-			//cv::FAST(bw3,keypoints3,20,false);			
-		}
-		catch(cv::Exception e)
-		{
-			std::cout << e.what();
-		}
-
-		std::cout << "FAST feature extraction: " << timer.getElapsed() << "\n";
-		
-		patches.clear();
-		for(int i=0;i<keypoints.size();i++)
-		{
-			//extract 11 by 11 patch around keypoint
-			std::vector<unsigned char> patch;
-			extract_patch(bw2,keypoints[i].pt,patch,11,0);
-			patches.push_back(patch);
-		}
-
-		std::cout << "patch extraction: " << timer.getElapsed() << "\n";		
-
-		Coordinate2DHashTable ht(bw2.rows,bw2.cols);
-
-		for(unsigned int i=0;i<lastKeypoints.size();i++)
-		{
-			arma::vec::fixed<2> coord;
-			coord[0] = lastKeypoints[i].pt.x;
-			coord[1] = lastKeypoints[i].pt.y;
-			ht.add(i,coord);
-		}
-
-		std::cout << "Build HashTable: " << timer.getElapsed() << "\n";
-
-		std::vector<int> matches;
-		for(unsigned int i=0;i<keypoints.size();i++)
-		{
-			arma::vec::fixed<2> coord;
-			coord[0] = keypoints[i].pt.x;
-			coord[1] = keypoints[i].pt.y;
-			std::vector<unsigned int> indices;
-			ht.getInRadius(coord,30,indices);
-
-			double bestScore = 5000;
-			int bestIndex = -1;
-			for(unsigned int j=0;j<indices.size();j++)
-			{
-				double score = ZMSSD(patches[i],lastPatches[indices[j]]);
-				if(score < bestScore)
-				{
-					bestScore = score;
-					bestIndex = indices[j];
-				}
-			}
-			matches.push_back(bestIndex);
-		}
-
-		std::vector<std::vector<int>> asdf(lastKeypoints.size());
-		for(int i=0;i<matches.size();i++)
-		{			
-			if(matches[i] >= 0)
-			{
-				asdf[matches[i]].push_back(i);
-			}
-		}
-
-		for(int i=0;i<asdf.size();i++)
-		{
-			double bestScore = 2000000;
-			int bestIndex = -1;
-			for(int j=0;j<asdf[i].size();j++)
-			{
-				double score = ZMSSD(patches[asdf[i][j]],lastPatches[i]);
-				if(score < bestScore)
-				{
-					bestScore = score;
-					bestIndex = asdf[i][j];
-				}
-			}
-			for(int j=0;j<asdf[i].size();j++)
-			{
-				if(asdf[i][j] != bestIndex)
-				{
-					matches[asdf[i][j]] = -1;
-				}
-			}
-		}
-
-		std::cout << "Find matches in radius: " << timer.getElapsed();			
-
-		for(int i=0;i<keypoints.size();i++)
-		{
-			//arma::vec::fixed<2> coord;
-			//coord[0] = keypoints[keypoints.size()/2].pt.x;
-			//coord[1] = keypoints[keypoints.size()/2].pt.y;
-			//std::vector<unsigned int> indices;
-			//ht.getInRadius(coord,40,indices);
-
-			//cv::line(image2,keypoints[i].pt,keypoints[i].pt,cv::Scalar(255,0,0),4);			
-			//cv::line(image2,keypoints[keypoints.size()/2].pt,keypoints[keypoints.size()/2].pt,cv::Scalar(0,255,0),3);
-			//for(int i=0;i<indices.size();i++)
-			//{
-				//cv::line(image2,lastKeypoints[indices[i]].pt,lastKeypoints[indices[i]].pt,cv::Scalar(0,0,255),2);
-			//}
-			
-			if(matches[i] >= 0)
-			{
-				cv::line(image2,keypoints[i].pt,lastKeypoints[matches[i]].pt,cv::Scalar(0,0,255),2);
-			}			
-		}
-		*/
-		/*
-		for(int i=0;i<keypoints0.size();i++)
-		{
-			cv::line(image0,keypoints0[i].pt,keypoints0[i].pt,cv::Scalar(255,0,0),2);
-		}
-		for(int i=0;i<keypoints1.size();i++)
-		{
-			cv::line(image1,keypoints1[i].pt,keypoints1[i].pt,cv::Scalar(255,0,0),2);
-		}
-		for(int i=0;i<keypoints2.size();i++)
-		{
-			cv::line(image2,keypoints2[i].pt,keypoints2[i].pt,cv::Scalar(255,0,0),2);
-		}
-		for(int i=0;i<keypoints3.size();i++)
-		{
-			cv::line(image3,keypoints3[i].pt,keypoints3[i].pt,cv::Scalar(255,0,0),2);
-		}
-		*/
-
-		//lastKeypoints = keypoints;
-		//lastPatches = patches;
-		//lastDescriptors = descriptors;
-
-		//arma::vec2 test;
-		//test << 1.0 << 2.0;
-
-		//currentFrame->addProjection(test);
-
 		//cv::imshow("Image",image2);
 		//cv::imshow("Level 0",image0);
 		//cv::imshow("Level 1",image1);
 		//cv::imshow("Level 2",image2);
 		//cv::imshow("Level 3",image3);
-
-		//lastKeypoints = keypoints;
-		//lastPatches = patches;
 
 		if(cv::waitKey(30) >= 0)
 		{
