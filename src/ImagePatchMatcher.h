b7902ca477fd2cd4aa9c982d352429831225fd8d
Andrew Floren <andrewfloren@gmail.com>
2011-10-27 00:09:42 -0500

    renamed a few classes

--- src/ImagePatchMatcher.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "FeatureMatcher.h"
-
-class ImagePatchMatcher : public FeatureMatcher
-{
-public:
-	void match(std::vector<Feature*> features1, std::vector<Feature*> features2, std::vector<int> &matches);	
-};
